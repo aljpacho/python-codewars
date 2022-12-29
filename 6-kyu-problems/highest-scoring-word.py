72652c3667e4678fabecfa8c5b197a829084f6e7
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-12-29 21:34:26 +0000

    Completed solution

--- 6-kyu-problems/highest-scoring-word.py
+++ 6-kyu-problems/highest-scoring-word.py
@@ -8,3 +8,23 @@ If two words score the same, return the word that appears earliest in the origin
 
 All letters will be lowercase and all inputs will be valid."""
 
+def high(x):
+    letter_scores = {chr(i+96):i for i in range(1,27)}
+    
+    words = x.split(' ')
+
+    scores = []
+    
+    for word in words:
+        score = 0
+        for char in word:
+            score += letter_scores[char]
+        scores.append(score)
+    
+    word_scores = list(zip(words, scores))
+    
+    max_word_score = max(word_scores, key=lambda x: x[1])
+    
+    return max_word_score[0]
+    
+    

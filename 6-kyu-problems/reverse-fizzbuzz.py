52da3b8d481d1e51e081f645f716b1d25e11b016
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-12-29 21:35:44 +0000

    Added problem description

---
+++ 6-kyu-problems/reverse-fizzbuzz.py
@@ -0,0 +1,9 @@
+"""FizzBuzz is often one of the first programming puzzles people learn. Now undo it with reverse FizzBuzz!
+
+Write a function that accepts a string, which will always be a valid section of FizzBuzz. Your function must return an array that contains the numbers in order to generate the given section of FizzBuzz.
+
+Notes:
+
+If the sequence can appear multiple times within FizzBuzz, return the numbers that generate the first instance of that sequence.
+All numbers in the sequence will be greater than zero.
+You will never receive an empty sequence."""

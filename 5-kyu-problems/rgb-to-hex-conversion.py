3148f321c82b861e92acc76089435cae1553659a
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2023-01-01 19:54:56 +0000

    Added solution to problem

--- 5-kyu-problems/rgb-to-hex-conversion.py
+++ 5-kyu-problems/rgb-to-hex-conversion.py
@@ -5,3 +5,16 @@ Note: Your answer should always be 6 characters long, the shorthand with 3 will 
 
 The following are examples of expected output values:
 """
+
+def rgb(r, g, b):
+    # Clamp values to range 0-255
+    r = min(max(r, 0), 255)
+    g = min(max(g, 0), 255)
+    b = min(max(b, 0), 255)
+    
+    # Convert RGB to Hex values
+    hex_r = hex(r)[2:].rjust(2, "0").upper()
+    hex_g = hex(g)[2:].rjust(2, "0").upper()
+    hex_b = hex(b)[2:].rjust(2, "0").upper()
+    
+    return hex_r + hex_g + hex_b

caf306dc9b76cebe4bb07f6e91b674b76cbfe12f
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2023-01-03 17:31:51 +0000

    Added best practice from discussion

--- 6-kyu-problems/are-they-the-same.py
+++ 6-kyu-problems/are-they-the-same.py
@@ -60,3 +60,10 @@ def comp(array1: list, array2: list) -> bool:
             return False
     
     return True
+
+# Best practice from discussion
+def comp(array1, array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
+        return False

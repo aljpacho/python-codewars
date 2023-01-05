74851dfa234f7f13ca00f1bec1f09444756a950a
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2023-01-05 21:06:36 +0000

    Added solution to problem

--- 4-kyu-problems/permutations.py
+++ 4-kyu-problems/permutations.py
@@ -10,3 +10,23 @@ Examples:
 * Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 
 The order of the permutations doesn't matter."""
+
+def permutations(string: str) -> set:
+    # Base case: if the input string is of length 1, return the string as a single-item list
+    if len(string) == 1:
+        return [string]
+
+    # Initialize an empty list to store the permutations
+    perms = []
+
+    # Iterate through each character in the string
+    for i, char in enumerate(string):
+        # Generate the permutations for the string without the current character
+        sub_perms = permutations(string[:i] + string[i+1:])
+        
+        # Add the current character to the beginning of each permutation and append to the list
+        for perm in sub_perms:
+            perms.append(char + perm)
+    
+    # Return the list of permutations as a set to remove duplicates
+    return set(perms)

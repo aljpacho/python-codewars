a236b5cf8dc64f2cbc686cdcc6f1da74d738a5b0
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2023-01-21 06:13:04 +0000

    Added solution to problem

--- 4-kyu-problems/explosive-sum.py
+++ 4-kyu-problems/explosive-sum.py
@@ -7,3 +7,20 @@ How many ways can you make the sum of a number?
 2 + 1 + 1
 1 + 1 + 1 + 1
 """
+
+def exp_sum(n: int) -> int:
+    """Calculates the number of integer partitions for a given
+    integer n using memoization
+
+    Args:
+        n (int): positive integer
+
+    Returns:
+        int: the number of integer partitions for n
+    """
+    partitions = [0] * (n + 1)
+    partitions[0] = 1
+    for i in range(1, n + 1):
+        for j in range(i, n + 1):
+            partitions[j] += partitions[j - i]
+    return partitions[n]

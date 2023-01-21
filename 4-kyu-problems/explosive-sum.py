e2a7984905ca98322b159b5b77e1afe456398724
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2023-01-21 06:17:55 +0000

    Added best practice and clever from discussion

--- 4-kyu-problems/explosive-sum.py
+++ 4-kyu-problems/explosive-sum.py
@@ -24,3 +24,11 @@ def exp_sum(n: int) -> int:
         for j in range(i, n + 1):
             partitions[j] += partitions[j - i]
     return partitions[n]
+
+# Best practice and clever solution 
+def exp_sum(n):
+    if n < 0: return 0
+    A = [1] + [0]*n
+    for i in range(n):
+        A = [sum(A[:k+1][::-i-1]) for k in range(n+1)]
+    return A[-1]

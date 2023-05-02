49296ce686f35535f4f16ba96b4f092b37423334
aljpacho <jordanpacho21@gmail.com>
2023-05-02 13:47:59 +0100

    Added type hinting

--- 8-kyu-problems/even-or-odd.py
+++ 8-kyu-problems/even-or-odd.py
@@ -1,4 +1,4 @@
 """Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers."""
 
-def even_or_odd(number):
+def even_or_odd(number:int) -> str:
     return "Even" if number % 2 == 0 else "Odd"
